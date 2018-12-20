e54082e5a680ea11c0aa5a826644de2ff0b43982
bncordier <mrbcordier@gmail.com>
2018-12-20 10:36:57 +0100

    array of dates for cards

--- src/app/common/calendar.service.ts
+++ src/app/common/calendar.service.ts
@@ -7,10 +7,29 @@ import { Observable } from 'rxjs';
 })
 export class CalendarService {
 
+  dates: any[] = [];
+
   constructor(private service: HttpClient) { }
 
   readAll(): Observable<any> {
     return this.service.get<any>('http://localhost:3000/api/calendar/events');
   }
+
+  getDateArray(): any {
+
+    const startDate = new Date(); //YYYY-MM-DD
+    const endDate = new Date(); //YYYY-MM-DD
+
+    endDate.setDate(endDate.getDate() + 30)
+
+
+    let dt = new Date(startDate);
+    while (dt <= endDate) {
+      
+      this.dates.push(new Date(dt));
+      dt.setDate(dt.getDate() + 1);
+    }
+    return this.dates;
+  };
 
 }
